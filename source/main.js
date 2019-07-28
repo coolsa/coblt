63bec0d235609a229271b03b133402af5a3b5feb
coolsa <me@cloudchagnon.xyz>
2019-07-28 00:09:25 -0600

    rainbow test

--- source/main.js
+++ source/main.js
@@ -5,7 +5,12 @@ define([
 		console.log("test2");
 		this.iso = new cubeView();
 		this.test();
-		this.iso.test('120deg',0,0,0);
+		for(var x=0;x<20;x++){
+			for(var y=0;y<20;y++){
+				var tint = (x+y)*20%360
+				this.iso.test((tint)+'deg',x,y,0);
+			}
+		};
 	};
 	main.prototype = {
 		test: function(){console.log("asdfasdf");}
